04b23d6808ea6108840915cb45129daa1387bc5a
mohammad yousuf pandith <54001362+yousufpandith@users.noreply.github.com>
2026-05-29 23:17:59 +0530

    Update osx_script.sh

--- ci_tools_atomic_dex/ci_scripts/osx_script.sh
+++ ci_tools_atomic_dex/ci_scripts/osx_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Workaround for https://github.com/actions/setup-python/issues/577
-
 brew update
 brew install autoconf \
             automake \
@@ -12,32 +11,47 @@ brew install autoconf \
             gnu-sed \
             coreutils \
             libtool \
-            gnu-getopt
+            gnu-getopt \
+            cmake \
+            llvm
 
 brew unlink python@3.12
-brew install llvm
-brew install ninja
-brew install cmake
 brew link --overwrite python@3.12
 
-pip3 install yq
-export CC=clang
-export CXX=clang++
-export MACOSX_DEPLOYMENT_TARGET=14.2
+# macOS 14 blocks global pip installs. Use --break-system-packages for CI environments.
+pip3 install yq --break-system-packages
 
-# get curl
-#git clone https://github.com/KomodoPlatform/curl.git
-#cd curl
-#git checkout curl-7_70_0
-#./buildconf
-#./configure --disable-shared --enable-static --without-libidn2 --without-ssl --without-nghttp2 --disable-ldap --with-darwinssl
-#make -j3 install
-#cd ../
+# Set up architecture and compiler variables for Apple Silicon (M-Series)
+export CC="/opt/homebrew/opt/llvm/bin/clang"
+export CXX="/opt/homebrew/opt/llvm/bin/clang++"
+export MACOSX_DEPLOYMENT_TARGET=14.0
 
+# FORCE CMake and vcpkg to target Apple Silicon instead of Intel
+export CMAKE_OSX_ARCHITECTURES="arm64"
+export VCPKG_DEFAULT_TRIPLET="arm64-osx"
+export CFLAGS="-arch arm64"
+export CXXFLAGS="-arch arm64"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+
+# If running in GitHub Actions, persist these variables to the next steps
+if [ -n "$GITHUB_ENV" ]; then
+  echo "CC=/opt/homebrew/opt/llvm/bin/clang" >> $GITHUB_ENV
+  echo "CXX=/opt/homebrew/opt/llvm/bin/clang++" >> $GITHUB_ENV
+  echo "MACOSX_DEPLOYMENT_TARGET=14.0" >> $GITHUB_ENV
+  echo "CMAKE_OSX_ARCHITECTURES=arm64" >> $GITHUB_ENV
+  echo "VCPKG_DEFAULT_TRIPLET=arm64-osx" >> $GITHUB_ENV
+  echo "CFLAGS=-arch arm64" >> $GITHUB_ENV
+  echo "CXXFLAGS=-arch arm64" >> $GITHUB_ENV
+  echo "LDFLAGS=-L/opt/homebrew/opt/llvm/lib" >> $GITHUB_ENV
+  echo "CPPFLAGS=-I/opt/homebrew/opt/llvm/include" >> $GITHUB_ENV
+fi
+
+# Build libwally-core (Now compiling for ARM64)
 git clone -b v0.8.5 https://github.com/KomodoPlatform/libwally-core.git --recurse-submodules
 cd libwally-core
 ./tools/autogen.sh
-./configure --disable-shared
+./configure --host=aarch64-apple-darwin --disable-shared
 sudo make -j3 install
 cd ..
 
